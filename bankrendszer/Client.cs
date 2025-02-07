22be9ace3093e1b90a8ef0c09160e88f8ffef11b
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2025-02-07 08:27:47 +0100

    v0.1 create Classes

---
+++ bankrendszer/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankrendszer
+{
+    internal class Client
+    {
+        public string Name { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public string Gender { get; private set; }
+        public string Email { get; private set; }
+
+        public Client(string name, DateTime dateOfBirth, string gender, string email)
+        {
+            this.Name = name;
+            this.DateOfBirth = dateOfBirth;
+            this.Gender = gender;
+            this.Email = email;
+        }
+    }
+}
